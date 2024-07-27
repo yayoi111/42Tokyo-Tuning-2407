df04992fab0f981664ce9c9420e0133bf6075b40
Hana Kobori <hakobori@c2r9s3.42tokyo.jp>
2024-07-27 20:02:20 +0900

    0_sample.sql migration

--- webapp/mysql/migration/0_sample.sql
+++ webapp/mysql/migration/0_sample.sql
@@ -1,4 +1,4 @@
 -- このファイルに記述されたSQLコマンドが、マイグレーション時に実行されます。
 ALTER TABLE sessions
-ADD CONSTRAINT user_id
+ADD CONSTRAINT fk_sessions_user_id
 FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE;
--- webapp/mysql/migration/1_sample.sql
+++ webapp/mysql/migration/1_sample.sql
@@ -1,7 +1,7 @@
-ALTER TABLE dispatchers
-ADD CONSTRAINT user_id
-FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE;
+-- ALTER TABLE dispatchers
+-- ADD CONSTRAINT fk_dispatchers_user_id
+-- FOREIGN KEY (use) REFERENCES users(id) ON DELETE CASCADE;
 
-ALTER TABLE dispatchers
-ADD CONSTRAINT area_id
-FOREIGN KEY (area_id) REFERENCES area(id);
+-- ALTER TABLE dispatchers
+-- ADD CONSTRAINT fk_dispatchers_area_id
+-- FOREIGN KEY (area_id) REFERENCES areas(id);
